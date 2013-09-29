ac65acbf79c320e535dc4332c9bd00c0e2e19f62
franz-ru <franz.ru@gmail.com>
2013-09-29 14:08:42 +0600

    auth check

--- djangodash2013/apps/core/views.py
+++ djangodash2013/apps/core/views.py
@@ -5,10 +5,13 @@ from django.shortcuts import resolve_url
 
 
 def main(request, template=u'main.html'):
-    friends = get_facebook_friends(request)
-    friends_by_sex = dict()
-    friends_by_sex[u'male'] = len([1 for friend in friends if friend['sex'] == u'male']) * 100.0 / len(friends)
-    friends_by_sex[u'female'] = len([1 for friend in friends if friend['sex'] == u'female']) * 100.0 / len(friends)
+    if request.user.is_authenticated():
+        friends = get_facebook_friends(request)
+        friends_by_sex = dict()
+        friends_by_sex[u'male'] = len([1 for friend in friends if friend['sex'] == u'male']) * 100.0 / len(friends)
+        friends_by_sex[u'female'] = len([1 for friend in friends if friend['sex'] == u'female']) * 100.0 / len(friends)
+    else:
+        friends_by_sex = dict()
     context = dict(
         friends_by_sex=friends_by_sex
     )
